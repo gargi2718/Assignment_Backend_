c9dbf1d85653169232c53d5e23f80646033f9e8f
Gargi Chandrakar <74736711+gargi2718@users.noreply.github.com>
2024-01-15 18:50:38 +0530

    Updated Changes- Redis Caching

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const rateLimit = require("express-rate-limit");
 const { DBConnection } = require("./database/db");
@@ -5,8 +6,21 @@ const postRoutes = require("./routes/route");
 const app = express();
 const port = process.env.PORT || 8000;
 
-//use database
-DBConnection();
+const redis = require("redis");
+let redisClient;
+
+if (process.env.NODE_ENV === "production") {
+  const redis = require("redis");
+  redisClient = redis.createClient();
+
+  redisClient.on("error", (err) => {
+    console.error("Redis error:", err);
+  });
+} else {
+  // Use the mock client for development
+  const { createClient } = require("redis-mock");
+  redisClient = createClient();
+}
 
 // Rate limiting middleware
 const limiter = rateLimit({
@@ -19,9 +33,19 @@ app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Pass Redis client to routes
+app.use((req, res, next) => {
+  req.redisClient = redisClient;
+  next();
+});
+
+//use database
+DBConnection();
 //use router
 app.use("/", postRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+module.exports={redisClient}
