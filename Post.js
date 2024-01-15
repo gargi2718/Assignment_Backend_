e8887f300b375f4cf5929d7faf1ae4650f090fde
Gargi Chandrakar <74736711+gargi2718@users.noreply.github.com>
2024-01-15 20:19:37 +0530

    Delete Post.js

--- Post.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const postSchema = new mongoose.Schema({
-  postId: { type: String, unique: true, required: true },
-  text: { type: String, required: true },
-});
-
-module.exports = mongoose.model("Post", postSchema);
